c84db1fa558f303c997f93ddc326fd20f26d83f9
Apokoala <michael_gazaway@hotmail.com>
2022-10-09 00:48:43 -0400

    initial commit

---
+++ data/server.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// require in express is like imports
+const express = require('express');
+const cors = require('cors');
+const { response } = require('express');
+require('dotenv').config();
+
+// create an instance of the express server.
+const app = express();
+
+app.arguments(cors());
+
+const PORT = process.env.PORT || 3002;
+
+app.get('/', (req, res) => {
+    response.send()
+})
+
+app.get('map????', (req, res) => {
+
+const type = req.query.(haventdecidedyet);
+res.send(also something temp);
+});
+
+class List {
+    constructor(type){
+        let {} = lists.lists.find(list)
+    }
+}
+
+

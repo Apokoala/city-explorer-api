66c285f526903b3e2412e2e14faa3b877fc35ad3
Apokoala <michael_gazaway@hotmail.com>
2022-10-12 22:22:58 -0400

    api commit

--- data/server.js
+++ data/server.js
@@ -3,30 +3,50 @@
 // require in express is like imports
 const express = require('express');
 const cors = require('cors');
-const { response } = require('express');
 require('dotenv').config();
 
+const weatherData = require('./data/weather.json');
 // create an instance of the express server.
 const app = express();
 
-app.arguments(cors());
+app.use(cors());
 
 const PORT = process.env.PORT || 3002;
 
 app.get('/', (req, res) => {
-    response.send()
-})
 
-app.get('map????', (req, res) => {
-
-const type = req.query.(haventdecidedyet);
-res.send(also something temp);
+    res.send('testing...it works')
 });
 
-class List {
-    constructor(type){
-        let {} = lists.lists.find(list)
+app.get('/weather', (req, res) => {
+    try{
+   const {lat, lon, searchQuery} = request.query;
+   const forecast = new Forecast(searchQuery);
+   const forecastArray = forecast.getForecast();
+   ResizeObserver.status(200).send(forecastArray);
+    } catch (error) {
+        console.log(error);
+        next(error.message0);
+    }
+})
+
+class Forecast {
+    constructor(cityWeSearchedFor){
+        let {data} = weatherData.find(city => city.city_name.toLowerCase() ===cityWeSearchedFor.toLowerCase);
+    }
+
+    getForecast() {
+        return this.data.map(day =>({
+            date: day.datetime,
+            description: day.weather.description
+        }))
     }
 }
+app.use((error, request, reponse, next) =>{
+    console.log(error);
 
+})
+
+
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
 
